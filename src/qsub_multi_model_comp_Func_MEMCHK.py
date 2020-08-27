cef945fa4088a62af5e77045d2c6984eedc1f559
Eric <69522926+werdna-spatial@users.noreply.github.com>
2020-08-27 16:01:43 -0400

    Create qsub_multi_model_comp_Func_MEMCHK.py
    modification to monitor memory.  Slows model down.

---
+++ src/qsub_multi_model_comp_Func_MEMCHK.py
@@ -0,0 +1,19 @@
+import tracemalloc
+from multi_model_comp_F import Model_Run 
+import argparse
+
+parser = argparse.ArgumentParser()
+parser.add_argument("RUN_ID")
+parser.add_argument("VALnits")
+parser.add_argument("VALpits")
+parser.add_argument("VALburnin")
+parser.set_defaults(RUN_ID=123456789,VALnits=1000,VALpits=100,VALburnin=500)
+args = parser.parse_args()
+
+tracemalloc.start()
+print("RUN_ID,VALnits,VALpits,VALburnin")
+print(args.RUN_ID,args.VALnits,args.VALpits,args.VALburnin, sep=',')
+Model_Run(str(args.RUN_ID),int(args.VALnits),int(args.VALpits),int(args.VALburnin))
+current, peak = tracemalloc.get_traced_memory()
+print(f"Current memory usage is {current / 10**6}MB; Peak was {peak / 10**6}MB")
+tracemalloc.stop()
