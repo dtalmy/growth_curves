bce41d67c31d4ea95a7a62d39aea30ec5d6142a8
Spiridon Papoulis <spapouli@vols.utk.edu>
2020-09-01 14:28:17 -0400

    SIn class now can parallelize the fitting procedure. Check fitting_benchmark.ipynb for benchmark

--- src/Infection_Models.py
+++ src/Infection_Models.py
@@ -1,26 +1,26 @@
 
 import numpy as np
 
-def zero_i(u,t,ps):
+def zero_i(y,t,ps):
     mum,phi,beta=ps[0],ps[1],ps[2]
-    S,V = u[0],u[1]
+    S,V = y[0],y[1]
     dSdt = mum*S - phi*S*V
     dVdt = beta*phi*S*V - phi*S*V
     return [dSdt,dVdt]
 
 # one infected classes
-def one_i(u,t,ps):
+def one_i(y,t,ps):
     mum,phi,beta,lam=ps[0],ps[1],ps[2],ps[3]
-    S,I1,V = u[0],u[1],u[2]
+    S,I1,V = y[0],y[1],y[2]
     dSdt = mum*S - phi*S*V
     dI1dt = phi*S*V - lam*I1
     dVdt = beta*lam*I1 - phi*S*V
     return [dSdt,dVdt,dI1dt]
 
 # five infected classes
-def two_i(u,t,ps):
+def two_i(y,t,ps):
     mum,phi,beta,lam,tau1=ps[0],ps[1],ps[2],ps[3],ps[4]
-    S,V,I1,I2 = u[0],u[1],u[2],u[3]
+    S,V,I1,I2 = y[0],y[1],y[2],y[3]
     dSdt = mum*S - phi*S*V
     dI1dt = phi*S*V - I1/tau1
     dI2dt = I1/tau1 - lam*I2
@@ -28,9 +28,9 @@ def two_i(u,t,ps):
     return [dSdt,dVdt,dI1dt,dI2dt]
 
 # five infected classes
-def three_i(u,t,ps):
+def three_i(y,t,ps):
     mum,phi,beta,tau1,tau2,lam=ps[0],ps[1],ps[2],ps[3],ps[4],ps[5]
-    S,V,I1,I2,I3 = u[0],u[1],u[2],u[3],u[4]
+    S,V,I1,I2,I3 = y[0],y[1],y[2],y[3],y[4]
     dSdt = mum*S - phi*S*V
     dI1dt = phi*S*V - I1/tau1
     dI2dt = I1/tau1 - I2/tau2
@@ -39,9 +39,9 @@ def three_i(u,t,ps):
     return [dSdt,dVdt,dI1dt,dI2dt,dI3dt]
 
 # five infected classes
-def four_i(u,t,ps):
+def four_i(y,t,ps):
     mum,phi,beta,lam,tau1,tau2,tau3=ps[0],ps[1],ps[2],ps[3],ps[4],ps[5],ps[6]
-    S,V,I1,I2,I3,I4 = u[0],u[1],u[2],u[3],u[4],u[5]
+    S,V,I1,I2,I3,I4 = y[0],y[1],y[2],y[3],y[4],y[5]
     dSdt = mum*S - phi*S*V
     dI1dt = phi*S*V - I1/tau1
     dI2dt = I1/tau1 - I2/tau2
@@ -51,9 +51,9 @@ def four_i(u,t,ps):
     return [dSdt,dVdt,dI1dt,dI2dt,dI3dt,dI4dt]
 
 # five infected classes
-def five_i(u,t,ps):
+def five_i(y,t,ps):
     mum,phi,beta,lam,tau1,tau2,tau3,tau4=ps[0],ps[1],ps[2],ps[3],ps[4],ps[5],ps[6],ps[7]
-    S,V,I1,I2,I3,I4,I5 = u[0],u[1],u[2],u[3],u[4],u[5],u[6]
+    S,V,I1,I2,I3,I4,I5 = y[0],y[1],y[2],y[3],y[4],y[5],y[6]
     dSdt = mum*S - phi*S*V
     dI1dt = phi*S*V - I1/tau1
     dI2dt = I1/tau1 - I2/tau2
--- src/Trait_SIn.py
+++ src/Trait_SIn.py
@@ -2,8 +2,22 @@ import numpy as np
 import pandas as pd
 from scipy.integrate import odeint
 import pylab
+import multiprocessing
 
 from .Infection_Models import zero_i,one_i,two_i,three_i,four_i,five_i
+
+def pool_worker(SImod,nits,burnin):
+    '''Function called by pool for parallized fitting'''
+    posterior = SImod._fit(nits,burnin,False)
+    return(posterior)
+
+def rawstats(pdseries):
+    '''calculates raw median and standard deviation of posterior'''
+    log_mean = np.log(pdseries).mean()
+    median = np.exp(log_mean)
+    log_std = np.log(pdseries).std()
+    std = ((np.exp(log_std**2)-1)*np.exp(2*log_mean+log_std**2.0))**0.5
+    return(median,std)
 
 class SIn():
 
@@ -38,11 +52,12 @@ class SIn():
         self.lam=lam
         self.tau= self.set_tau(tau)
 
-        self.fittings = None
-    def get_statevar(self):
+    def get_numstatevar(self):
+        '''returns the number of state varaibles'''
         return(self.Istates + 2)
 
     def __repr__(self):
+        '''pretty printing'''
         outstr = ["Number of Infection States = {}".format(self.Istates),
                 "Parameters:",
                 "\tmu = {}".format(self.mu),
@@ -57,15 +72,13 @@ class SIn():
             t = [str(t) for t in self.tau]
             outstr.append("\ttau = {}".format(', '.join(t)))
 
-        if self.fittings:
-            outstr.append('\nFor fittings, see fittings attribute')
-
         return('\n'.join(outstr))
 
     def __str__(self):
         return(self.__repr__())
 
     def get_pnames(self):
+        '''returns the names of the variables used in the current model'''
         names = ['mu','phi','beta']
         if self.Istates > 0:
             names.append('lam')
@@ -97,9 +110,16 @@ class SIn():
         else:
             taus = None
         return(taus)
-    
-    def get_inits(self,host_init=None,vioutdat
-            specify inital abundance of virus, otherwise, t0 from experimental data will be used
+
+    def get_inits(self,host_init=None,virus_init=None):
+        '''get the initial state variable values for integration
+
+        Parameters
+        ----------
+        host_init : int, optional
+            ignore h0 in data and set the host initial value
+        virus_init : int, optional
+            ignore v0 in data and set the viral initial value
 
         Return
         ------
@@ -135,22 +155,62 @@ class SIn():
             return(five_i)
         raise Exception("A {} infection state model must be implemented".format(self.Istates))
 
-    def get_parameters(self):
-        '''return the parameters needed for integration'''
+    def get_parameters(self,asdict=False):
+        '''return the parameters needed for integration
+        
+        Parameters
+        ----------
+        asdict : bool, optional
+            If true, return dict with parameter names mapped to values
+    
+        Return
+        ------
+        parameters
+            numpy array of parameters or dict of parameters
+        '''
         ps = [self.mu,self.phi,self.beta]
         if self.Istates == 1:
             ps.append(self.lam)
         if self.Istates > 1:
             ps.append(self.tau)
-        return(tuple([np.r_[ps]]))
+        ps=tuple([np.r_[ps]])
+        if asdict:
+            mapping = {}
+            for i,name in enumerate(self.get_pnames()):
+                mapping[name] = ps[0][i]
+            return(mapping)
+        return(ps)
+
+    def set_parameters(self,**parameters):
+        '''set parameters for the model'''
+        for p in parameters:
+            if p =='mu':
+                self.mu = parameters[p]
+            elif p =='phi':
+                self.phi = parameters[p]
+            elif p =='beta':
+                self.beta = parameters[p]
+            elif p=='lam':
+                self.lam = parameters[p]
+            elif 'tau' == p:
+                self.taus = self.set_tau(parameters[p])
+            else:
+                if 'tau' == p[0:3]:
+                    i = int(p[3:])
+                    self.taus[i+1] = parameters[p]
+
 
     def integrate(self,inits=None,parameters=None,forshow=True):
         '''allows option to return model solutions at sample times
 
         Parameters
         ----------
+        inits : numpy.array, optional
+            ignore h0 and v0 in data and set the initial values for integration
+        parameters : numpy.array, optional
+            ignore stored parameters and use specified
         forshow : bool
-            no idea right now
+            pick predicted values at times in data
 
         Returns
         -------
@@ -173,6 +233,7 @@ class SIn():
         return h,v
 
     def get_chi(self,h,v):
+        '''calculate chi values from predicted values'''
         chi = sum((np.log(h) - np.log(self._ha)) ** 2 / \
                     (np.log(1.0+self._hu**2.0/self._ha**2.0)**0.5 ** 2)
                     ) \
@@ -182,11 +243,13 @@ class SIn():
         return(chi)
 
     def get_AIC(self,parcount,h,v):
+        '''calcualte AIC for the model fit'''
         K = parcount
         AIC = -2*np.log(np.exp(-self.get_chi(h,v))) + 2*K
         return(AIC)
 
     def get_rsquared(self,h,v):
+        '''calculate R^2'''
         ssres = sum((h - self._ha) ** 2) \
             + sum((v - self._va) ** 2)
         sstot = h.shape[0]*np.var(self._ha) \
@@ -195,21 +258,53 @@ class SIn():
 
 
     def get_adjusted_rsquared(self,h,v,samples,parcount):
+        '''calculate adjusted R^2'''
         R2 = self.get_rsquared(h,v)
         n = samples
         p = parcount
         adjR2 = 1 - (1-R2)*(n-1)/(n-p-1)
         return adjR2
 
-    def fit(self,nits=1000,pits=100,burnin=500):
+    def _parallel_fit(self,nits,cores):
+        '''parallelized fitting procedure
+        
+        Parameters
+        ----------
+        nits : int
+            number of iterations
+        cores : int
+            number of cores to use in fitting
+        
+        Returns
+        -------
+        fittings
+            list of pandas.DataFrame objects with fittings
+        
+        ''''
+        if cores > multiprocessing.cpu_count():
+            Warning("More cores specified than avalible, cpu_cores set to maximum avalible\n")
+            cores=multiprocessing.cpu_count()
+        print("Starting fitting procedure with {} cores".format(cores))
+        jobs = []
+        jnits = int(nits/cores)
+        jbern = int(jnits/2)
+        for i in range(0,cores):
+            mod = SIn(self.df)
+            mod.set_parameters(**self.get_parameters(asdict=True))
+            jobs.append([mod,jnits,jbern])
+        with multiprocessing.Pool(processes=cores) as pool:
+            results = pool.starmap(pool_worker,jobs)
+        pool.join()
+        pool.close()
+        return(results)
+
+    def _fit(self,nits=1000,burnin=500,print_progress=True):
         '''allows option to return model solutions at sample times
 
         Parameters
         ----------
         nits : int
             number of iterations
-        pits : int
-            number of iterations to print
         burnin : int
             number of iterations to ignore initially
 
@@ -228,14 +323,17 @@ class SIn():
         ars, likelihoods = np.r_[[]], np.r_[[]]
         opt = np.ones(npars)
         stds = np.zeros(npars) + 0.05
-        pall = np.r_[[np.zeros(nits-burnin) for i in range(npars)]]
-        iterations = np.arange(1, nits, 1)
-        chi = self.get_chi(h,v)
-        
-        outdat = []
 
-        print('a priori error', chi)
-        print('iteration; ' 'error; ' 'acceptance ratio')
+        #defining the number of iterations
+        iterations = np.arange(1, nits, 1)
+        #initial prior
+        chi = self.get_chi(h,v)
+        pall = []
+        #print report and update output
+        pits = int(nits/10)
+        if print_progress:
+            print('a priori error', chi)
+            print('iteration; ' 'error; ' 'acceptance ratio')
         for it in iterations:
             pars_old = pars
             pars = np.exp(np.log(pars) + opt*pylab.normal(0, stds, npars))
@@ -245,8 +343,7 @@ class SIn():
             if np.exp(chi-chinew) > pylab.rand():  # KEY STEP
                 chi = chinew
                 if it > burnin:  # only store the parameters if you've gone through the burnin period
-                    pall[:,ic] = pars
-                    outdat.append(np.append(pars,
+                    pall.append(np.append(pars,
                                             [chi,self.get_adjusted_rsquared(h,v,samples,npars),it]
                                             )
                                 )
@@ -254,16 +351,52 @@ class SIn():
                     ic = ic + 1  # total count
             else: #if chi gets worse, use old parameters
                 pars = pars_old
-            if (it % pits == 0):
+            if (it % pits == 0) and print_progress:
                 print(it,';', round(chi,2),';', ar/pits)
                 ars = np.append(ars, ar/pits)
                 ar = 0.0
         likelihoods = likelihoods[burnin:]
         iterations = iterations[burnin:]
-        pall = pall[:,:ic]
+        #pall = pall[:,:ic]
         #print_posterior_statistics(pall,pnames)
-        outdat = pd.DataFrame(outdat)
-        outdat.columns = list(pnames)+['chi','adjR2','Iteration']
-        self.fittings=outdat
-        return pall,likelihoods,iterations,outdat
+        df = pd.DataFrame(pall)
+        df.columns = list(pnames)+['chi','adjR2','Iteration']
+        return df
     
+    def fit(self,iterations,cpu_cores=1,print_report=True):
+        '''fits parameters to dataframe
+
+        Parameters
+        ----------
+        iterations : int
+            number of iterations
+        cpu_cores : int, optional
+            number of cores used in fitting, Default = 1
+        print_report : bool
+            Print a basic
+
+        Returns
+        -------
+        tupple : pall, likelihoods, iterations
+            host and virus counts
+
+        '''
+        if cpu_cores == 1:
+            print("Starting fitting procedure with 1 core")
+            posterior = self._fit(iterations,int(iterations/2),False)
+        else:
+            posterior_list=self._parallel_fit(iterations,cpu_cores)
+            posterior = pd.concat(posterior_list)
+        p_median= {}
+        report=["\nFitting Report\n==============="]
+        for col in list(self.get_pnames()):
+            median,std = rawstats(posterior[col])
+            report.append("parameter: {}\n\tmedian = {:0.3e},Standard deviation = {:0.3e}".format(col,median,std))
+            p_median[col]=median
+        
+        self.set_parameters(**p_median) #reset params with new fits
+        
+        if print_report:
+            print('\n'.join(report))
+        
+        return(posterior)
