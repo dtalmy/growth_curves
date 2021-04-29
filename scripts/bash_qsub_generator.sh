fe0a101d0f62a3ead4edc0d6ea9ead123899d5cd
Eric <69522926+werdna-spatial@users.noreply.github.com>
2021-04-28 21:13:59 -0400

    Create bash_qsub_generator.sh

---
+++ scripts/bash_qsub_generator.sh
@@ -0,0 +1,39 @@
+#!/bin/bash
+
+declare -a arr=("Baudoux32"
+"Eissler40"
+"Kim52"
+"Nagasaki29"
+"Tomaru51"
+"Slagter2016"
+"Baudoux33"
+"Gao41"
+"Kim71"
+"Nagasaki42"
+"Tomaru56"
+"Slagter2016a"
+"Baudoux34"
+"Johannessen68"
+"Kim72"
+"Sandaa26"
+"Tomaru57"
+"Maat2016a"
+"Baudoux3"
+"Johannessen69"
+"Kimura54"
+"Tomaru39"
+"Toyoda53"
+"Maat2016b"
+"Castberg24"
+"Johannessen70"
+"Kimura66"
+"Tomaru43"
+"Steenhauer2016"
+"Kimura67"
+)
+
+for i in "${arr[@]}"
+do
+   echo "$i"
+   # or do whatever with individual element of the array
+done
