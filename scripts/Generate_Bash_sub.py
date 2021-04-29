5f9e51b9d4c53e7ae315086dd5c528967ee8272f
Eric <69522926+werdna-spatial@users.noreply.github.com>
2021-04-29 14:24:52 -0400

    Update Generate_Bash_sub.py

--- scripts/Generate_Bash_sub.py
+++ scripts/Generate_Bash_sub.py
@@ -10,15 +10,26 @@ def main(VALver):
     #########################################################
     # read data
     print(VALver)
-    
+    dest_file= 'Gen_sub'+str(VALver)+'.sh'
+    print(dest_file)
+    cmdresult=subprocess.run(["touch "+dest_file], capture_output=True, text=True)
+    print(cmdresult.stdout)
+    print(cmdresult.stderr)
+    result=subprocess.run(["ls", "-l"], capture_output=True, text=True)    
+    print(result.stdout)
+    cmdresult=subprocess.run(["chmod ug+x "+dest_file], capture_output=True, text=True)
+    print(cmdresult.stdout)
+    print(cmdresult.stderr)
+    result=subprocess.run(["ls", "-l"], capture_output=True, text=True)    
+    print(result.stdout)
+    #
     master_df = pd.read_csv('../data/input/processed/processed_data.csv',index_col='id')
     abiotic_treatment_df = pd.read_csv('../data/input/preprocessed/reu_2019/treatments.csv',index_col='id')
     abiotic_treatment_df = abiotic_treatment_df[abiotic_treatment_df['treatment']=='Replete']
     master_df = pd.concat((master_df,abiotic_treatment_df))
     treatments = master_df.query('control==False').copy() # remove controls
     tids = treatments.index.unique() # unique ids
-    dest_file= 'Gen_sub'+str(VALver)+'.sh'
-    print(dest_file)
+    
     with open(dest_file, 'w') as writer:
         writer.write('#!/bin/bash\n')
         writer.write('declare -a arr=(\n')
@@ -31,11 +42,6 @@ def main(VALver):
         writer.write('   echo \"$i\"\n')
         writer.write('   qsub qsub_tids.sh  -v VALtids=$i,VALoutpath=/lustre/haven/proj/UTK0105/Python_runs/aaa  \n')
         writer.write('done')
-    result=subprocess.run(["ls", "-l"], capture_output=True, text=True)    
-    print(result.stdout)
-    cmdresult=subprocess.run(["chmod ug+x "+dest_file], capture_output=True, text=True)
-    print(cmdresult.stdout)
-    print(cmdresult.stderr)
     result=subprocess.run(["ls", "-l"], capture_output=True, text=True)    
     print(result.stdout)
 
