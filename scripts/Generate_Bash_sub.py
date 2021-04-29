275e17942faccc52363bc79ded65e24d4dff3eab
Eric <69522926+werdna-spatial@users.noreply.github.com>
2021-04-29 14:59:08 -0400

    Update Generate_Bash_sub.py
    Clean up

--- scripts/Generate_Bash_sub.py
+++ scripts/Generate_Bash_sub.py
@@ -10,10 +10,13 @@ def main(VALver):
     #########################################################
     ## Single model run based on tids value
     #########################################################
-    # read data
-    print(VALver)
+    # Create file name
+    #
     dest_file= 'Sub_TIDS'+str(VALver)+'.sh'
     print(dest_file)
+    print('\n')
+    #
+    # read data
     #
     master_df = pd.read_csv('../data/input/processed/processed_data.csv',index_col='id')
     abiotic_treatment_df = pd.read_csv('../data/input/preprocessed/reu_2019/treatments.csv',index_col='id')
@@ -21,12 +24,17 @@ def main(VALver):
     master_df = pd.concat((master_df,abiotic_treatment_df))
     treatments = master_df.query('control==False').copy() # remove controls
     tids = treatments.index.unique() # unique ids
-    
+    print('Found tids:\n')
+    for did in tids:
+            print(did)
+    #
+    # Write bash script
+    # the with automatically closes the file
+    #
     with open(dest_file, 'w') as writer:
         writer.write('#!/bin/bash\n')
         writer.write('declare -a arr=(\n')
         for did in tids:
-            print(did)
             writer.write('\"'+did+'\"\n')
         writer.write(')\n')
         writer.write('for i in \"${arr[@]}\"\n')
@@ -34,12 +42,15 @@ def main(VALver):
         writer.write('   echo \"$i\"\n')
         writer.write('   qsub qsub_tids.sh  -v VALtids=$i,VALoutpath=/lustre/haven/proj/UTK0105/Python_runs/aaa  \n')
         writer.write('done')
-        
+    #
+    # Set file to be executible for both user and group
+    #    
+    print('\n')
     print("Set file permission for execute")
     result=subprocess.run(["ls", "-l",dest_file], capture_output=True, text=True)    
     print(result.stdout)
         
-    os.chmod(dest_file, stat.S_IRUSR | stat.S_IWUSR |stat.S_IXUSR |stat.S_IRGRP | stat.S_IROTH)
+    os.chmod(dest_file, stat.S_IRUSR | stat.S_IWUSR |stat.S_IXUSR |stat.S_IRGRP |stat.S_IWGRP |stat.S_IXGRP | stat.S_IROTH)
     
     result=subprocess.run(["ls", "-l",dest_file], capture_output=True, text=True)    
     print(result.stdout)
