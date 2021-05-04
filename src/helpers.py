e7998c4aca2a7ce62f215908466b9a8d86c86015
dtalmy <dtalmy@utk.edu>
2021-05-03 21:18:17 -0400

    increasing number of iterations

--- src/helpers.py
+++ src/helpers.py
@@ -103,7 +103,7 @@ def get_models(df):
 
 # retrieve posteriors
 def get_posteriors(model,chain_inits=2):
-    posteriors = model.MCMC(chain_inits=chain_inits,iterations_per_chain=10,
+    posteriors = model.MCMC(chain_inits=chain_inits,iterations_per_chain=1000000,
                        cpu_cores=2,fitsurvey_samples=10000,sd_fitdistance=20.0)
     return posteriors
 
