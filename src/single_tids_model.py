052997ce94a8090555cc1a8856925ad4362a6a54
dtalmy <dtalmy@utk.edu>
2021-12-09 10:55:56 -0500

    use helper function to load data

--- src/single_tids_model.py
+++ src/single_tids_model.py
@@ -13,24 +13,10 @@ def main(VALtids,DATAdir):
     # read data
     print(VALtids)
     print(DATAdir)
-    master_df = pd.read_csv('../data/input/processed/processed_data.csv',index_col='id')
-    abiotic_treatment_df = pd.read_csv('../data/input/preprocessed/reu_2019/treatments.csv',index_col='id')
-    abiotic_treatment_df = abiotic_treatment_df[abiotic_treatment_df['treatment']=='Replete']
-    nissimov_df = pd.read_csv('../data/input/preprocessed/nissimov/with_reps.csv',index_col='id')
-    nissimov_df['abundance'] = np.mean(np.r_[[nissimov_df[i] for i in ['rep1','rep2','rep3']]],axis=0)
-    master_df = pd.concat((master_df,abiotic_treatment_df,nissimov_df))
-    treatments = master_df.query('control==False').copy() # remove controls
-    #tids = treatments.index.unique() # unique ids
-
-    # process tids value in datset and save model output to pdf
     did = VALtids
     print(did)
-    df = treatments.loc[did].copy()
-    df.loc[:,'log_sigma'] = 0.2 # define uncertainty in data 
-    df.loc[df.organism == 'H', 'time'] = df.loc[df.organism == 'H', 'time'].copy() -\
-                min(df.loc[df.organism == 'H', 'time']) # remove non-zero initial timepoints
-    df.loc[df.organism == 'V', 'time'] = df.loc[df.organism == 'V', 'time'].copy() -\
-                min(df.loc[df.organism == 'V', 'time']) # same for virus
+    main_df = get_master_dataframe()
+    df = main_df.loc[did].copy()
     tpdf = fit_all(df) # here is where the main work is done
     tpdf.close()
     
